341d498e3f2815050ef1d2579d1dfd1aa3d56b7f
Utibe Effiong <utibenoaheffiong@gmail.com>
2026-05-22 22:00:22 +0100

    Add development and production errors

--- Middlewares/globalErrorMiddleware.js
+++ Middlewares/globalErrorMiddleware.js
@@ -1,11 +1,50 @@
+const prodErrors=(res,error)=>{
+
+    if (error.isOperational) {
+        res.status(error.statusCode).json({
+
+            status:error.statusCode,
+            message:error.message,
+
+        })
+    }else{
+        res.status(500).json({
+
+            status:'Fail',
+            message:'Something is wrong! try again',
+
+        })
+    }
+    
+}
+
+const devErrors=(res,error)=>{
+
+    res.status(error.statusCode).json({
+
+            status:error.statusCode,
+            message:error.message,
+            stack:error.stack,
+            error:error
+
+        })
+    
+}
+
+
+
+
+
+
+
 module.exports=(error,req,res,next)=>{
     error.statusCode=error.statusCode || 500
     error.status=error.status || 'error'
 
-    res.status(error.statusCode).json({
-
-        status:error.statusCode,
-        message:error.message
-
-    })
+    if (process.env.NODE_ENV==='development') {
+        devErrors(res, error)
+    }
+    else if(process.env.NODE_ENV==='production'){
+        prodErrors(res,error)
+    }
 }
